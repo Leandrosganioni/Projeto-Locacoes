3c34ba35646b6e35a79849eb717e6eb7be75ceb1
Leandro Sganioni <leandrosgani@gmail.com>
2025-06-10 19:57:10 -0300

    Update 2025_06_10_225632_add_imagem_to_equipamentos_table.php

--- Locacoes/database/migrations/2025_06_10_225632_add_imagem_to_equipamentos_table.php
+++ Locacoes/database/migrations/2025_06_10_225632_add_imagem_to_equipamentos_table.php
@@ -6,23 +6,17 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::table('equipamentos', function (Blueprint $table) {
-            //
+            $table->string('imagem')->nullable()->after('nome');
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::table('equipamentos', function (Blueprint $table) {
-            //
+            $table->dropColumn('imagem');
         });
     }
 };
