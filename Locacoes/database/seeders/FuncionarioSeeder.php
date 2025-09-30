e003acefc9bcf2e76d632038301fdcc54844cf59
Leandro Sganioni <leandrosgani@gmail.com>
2025-09-30 15:44:39 -0300

    Adicionar PHPDoc e reduzir a contagem de seeds em FuncionarioSeeder.
    Comentários PHPDoc adicionados para melhorar a documentação e a clareza do código. O número de registros de "Funcionário" gerados foi reduzido de 10 para 3, a fim de tornar os dados de teste mais gerenciáveis.

--- Locacoes/database/seeders/FuncionarioSeeder.php
+++ Locacoes/database/seeders/FuncionarioSeeder.php
@@ -1,23 +1,51 @@
 <?php
-
+/**
+ * FuncionarioSeeder file.
+ *
+ * Seeder for creating sample Funcionario records in the database.
+ *
+ * PHP version 8
+ *
+ * @category DatabaseSeeder
+ * @package  Database\Seeders
+ * @author   Leandro <leandrosgani@gmail.com>
+ * @license  MIT https://opensource.org/licenses/MIT
+ * @link     https://github.com/your-repo/Projeto-Locacoes
+ */
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\Funcionario;
 use Faker\Factory as Faker;
 
+/**
+ * Seeder for creating sample Funcionario records.
+ *
+ * @category DatabaseSeeder
+ * @package  Database\Seeders
+ * @author   Leandro <leandrosgani@gmail.com>
+ * @license  MIT https://opensource.org/licenses/MIT
+ * @link     https://github.com/your-repo/Projeto-Locacoes
+ */
 class FuncionarioSeeder extends Seeder
 {
+    /**
+     * Run the database seeds for Funcionario.
+     *
+     * @return void
+     */
     public function run(): void
     {
         $faker = Faker::create('pt_BR');
 
-        for ($i = 0; $i < 10; $i++) {
-            Funcionario::create([
-                'nome' => $faker->name,
-                'cpf' => $faker->cpf(false),
-                'telefone' => $faker->phoneNumber,
-            ]);
+        for ($i = 0; $i < 3; $i++) {
+            Funcionario::create(
+                [
+                    'nome' => $faker->name,
+                    'cpf' => $faker->cpf(false),
+                    'telefone' => $faker->phoneNumber,
+                ]
+            );
         }
     }
 }
