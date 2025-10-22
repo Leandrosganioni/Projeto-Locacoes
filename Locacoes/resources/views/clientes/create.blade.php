a1b5141ec3f3daee49c37f9f38be3fe305803b37
Leandro Sganioni <leandrosgani@gmail.com>
2025-10-22 12:52:17 -0300

    Update create.blade.php

--- Locacoes/resources/views/clientes/create.blade.php
+++ Locacoes/resources/views/clientes/create.blade.php
@@ -24,22 +24,25 @@
             {{-- Campos existentes --}}
             <div class="mb-3">
                 <label for="nome" class="form-label">Nome Completo</label>
-                {{-- old('nome') mantém o valor digitado se a validação falhar --}}
                 <input type="text" class="form-control @error('nome') is-invalid @enderror" id="nome" name="nome" value="{{ old('nome') }}" required>
-                {{-- Exibe erro específico para o campo nome --}}
                 @error('nome')
                     <div class="invalid-feedback">{{ $message }}</div>
                 @enderror
             </div>
             
             <div class="row mb-3">
+                
+                {{-- --- A CORREÇÃO ESTÁ AQUI --- --}}
+                {{-- O nome do campo (name) deve ser 'cpf_cnpj' para corresponder ao Controller e ao Banco de Dados --}}
                 <div class="col-md-6">
-                    <label for="cpf" class="form-label">CPF</label>
-                    <input type="text" class="form-control @error('cpf') is-invalid @enderror" id="cpf" name="cpf" placeholder="000.000.000-00" value="{{ old('cpf') }}" required>
-                     @error('cpf')
+                    <label for="cpf_cnpj" class="form-label">CPF/CNPJ</label>
+                    <input type="text" class="form-control @error('cpf_cnpj') is-invalid @enderror" id="cpf_cnpj" name="cpf_cnpj" placeholder="000.000.000-00 ou 00.000.000/0000-00" value="{{ old('cpf_cnpj') }}" required>
+                     @error('cpf_cnpj')
                         <div class="invalid-feedback">{{ $message }}</div>
                     @enderror
                 </div>
+                {{-- --- FIM DA CORREÇÃO --- --}}
+
                 <div class="col-md-6">
                     <label for="telefone" class="form-label">Telefone</label>
                     <input type="text" class="form-control @error('telefone') is-invalid @enderror" id="telefone" name="telefone" value="{{ old('telefone') }}" required>
@@ -57,15 +60,12 @@
                 @enderror
             </div>
 
-            {{-- --- Documentação (Novos Campos de Autenticação) --- --}}
-            {{-- Adicionamos uma seção para os dados de login do cliente --}}
             <hr class="my-4">
             <h5 class="mb-3 fw-semibold">Dados de Acesso (Login)</h5>
 
             <div class="row mb-3">
                 <div class="col-md-6">
                     <label for="email" class="form-label">E-mail</label>
-                    {{-- Campo para o e-mail que será usado para login --}}
                     <input type="email" class="form-control @error('email') is-invalid @enderror" id="email" name="email" value="{{ old('email') }}" required>
                      @error('email')
                         <div class="invalid-feedback">{{ $message }}</div>
@@ -76,7 +76,6 @@
             <div class="row mb-3">
                  <div class="col-md-6">
                     <label for="password" class="form-label">Senha</label>
-                    {{-- Campo para a senha --}}
                     <input type="password" class="form-control @error('password') is-invalid @enderror" id="password" name="password" required>
                      @error('password')
                         <div class="invalid-feedback">{{ $message }}</div>
@@ -84,9 +83,7 @@
                 </div>
                  <div class="col-md-6">
                     <label for="password_confirmation" class="form-label">Confirmar Senha</label>
-                    {{-- Campo para confirmação da senha. O nome 'password_confirmation' é importante para a validação do Laravel --}}
                     <input type="password" class="form-control" id="password_confirmation" name="password_confirmation" required>
-                    {{-- Não precisa de @error aqui, o Laravel valida a confirmação automaticamente --}}
                 </div>
             </div>
             
@@ -107,4 +104,3 @@
 @push('styles')
 <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet">
 @endpush
-
